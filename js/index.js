ca4832e95bac93c67b4bee8d30f9c42aada6a990
vincentbrian <vincentbrian.design@gmail.com>
2017-06-21 17:39:25 -0400

    add slick and carousel

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 
 
-$(".video-tile").click( function (){
-  console.log($(this).attr("data-video"));
+$(".video-tile").click( function (){            //add click handler to elements with class ".video-tile"
+  var videoUrl = $(this).attr("data-video");    //get attr "data-video" for the current element
+  var iframeElement = $("#video-iframe");       //get iframe element
+  var iframeSrc = iframeElement.attr("src");    //get iframe element attribute "src"
 
-  console.log($("#video-iframe").attr("src"));
-  // alert(this.id);
+videoUrl = videoUrl + "?autoplay=1";
+  console.log(videoUrl);
+  console.log(iframeSrc)
+  console.log(iframeElement);
+
+  iframeElement.attr("src", videoUrl )
 });
