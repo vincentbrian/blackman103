149229020533b24a0ee5691e5659056cffbae023
vincentbrian <vincentbrian.design@gmail.com>
2017-06-28 16:58:47 -0400

    carousel slick completed

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
+$(".video-tile").click(function() { //add click handler to elements with class ".video-tile"
+  var videoUrl = $(this).attr("data-video"); //get attr "data-video" for the current element
+  var iframeElement = $("#video-iframe"); //get iframe element
+  var iframeSrc = iframeElement.attr("src"); //get iframe element attribute "src"
 
-
-$(".video-tile").click( function (){            //add click handler to elements with class ".video-tile"
-  var videoUrl = $(this).attr("data-video");    //get attr "data-video" for the current element
-  var iframeElement = $("#video-iframe");       //get iframe element
-  var iframeSrc = iframeElement.attr("src");    //get iframe element attribute "src"
-
-videoUrl = videoUrl + "?autoplay=1";
+  videoUrl = videoUrl + "?autoplay=1";
   console.log(videoUrl);
   console.log(iframeSrc)
   console.log(iframeElement);
 
-  iframeElement.attr("src", videoUrl );
+  iframeElement.attr("src", videoUrl);
+});
 
-  $('#slick-carousel').slick();
+$('#slick-carousel').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
 });
