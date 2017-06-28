ca3ed3e22b6e90da79a2a12843c0c462f375ee6f
vincentbrian <vincentbrian.design@gmail.com>
2017-06-28 18:12:55 -0400

    carousel slick js video-tile videos

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-$(".video-tile").click(function() { //add click handler to elements with class ".video-tile"
+$(".video-carousel").on("click", ".video-tile", function() { //add click handler to elements with class ".video-tile"
   var videoUrl = $(this).attr("data-video"); //get attr "data-video" for the current element
   var iframeElement = $("#video-iframe"); //get iframe element
   var iframeSrc = iframeElement.attr("src"); //get iframe element attribute "src"
+
 
   videoUrl = videoUrl + "?autoplay=1";
   console.log(videoUrl);
@@ -13,6 +14,33 @@ $(".video-tile").click(function() { //add click handler to elements with class "
 
 $('#slick-carousel').slick({
   infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
