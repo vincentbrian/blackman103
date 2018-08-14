aa300393fae3be44a56824a0f1831680b66bc98c
vincentbrian <vincentbrian.design@gmail.com>
2018-08-14 15:00:33 -0400

    jQuery slideToggle navbar dropdown

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-// $(".hamburger").click(function(){
-//   alert("hello");
-// });
-//
-// $(".hamburger").click(function(){
-//     $(".navbar-links").toggleClass("hide", 6000);
-// });
-$(document).ready( function(){
-  $(".hamburger").click(function(){
-    $(".navbar-links").toggleClass("hide", 2000).animate(2000);
+
+$(document).ready(function() {
+  $(".hamburger").click(function() {
+      $(".navbar-links").slideToggle("hide");
+
   });
 });
